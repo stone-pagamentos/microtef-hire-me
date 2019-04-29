cc0a56391270686a4c641515ea0f4e929d41cd25
silvastefan <silva.stefan@gmail.com>
2019-04-29 00:15:45 -0300

    add Interfaces

---
+++ src/Karnak.Domain/Interfaces/ITransactionTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Karnak.Domain.Models;
+
+namespace Karnak.Domain.Interfaces
+{
+    public interface ITransactionTypeRepository : IRepository<TransactionType>
+    {
+        TransactionType GetByName(string name);
+    }
+}
---
+++ src/Karnak.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Karnak.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        bool Commit();
+    }
+}
