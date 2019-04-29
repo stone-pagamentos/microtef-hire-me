4b2adbf0daf643507081e393ee6e59dcb85d6b90
silvastefan <silva.stefan@gmail.com>
2019-04-29 00:14:10 -0300

    add application ViewModels

---
+++ src/Karnak.Application/ViewModels/CardTypeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Karnak.Application.ViewModels
+{
+    public class CardTypeViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is Required")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+    }
+}
