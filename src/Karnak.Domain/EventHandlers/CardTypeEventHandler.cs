7a868569824d9cb75e766911b32c23f79ee19700
silvastefan <silva.stefan@gmail.com>
2019-04-29 00:16:23 -0300

    add EventHandlers

---
+++ src/Karnak.Domain/EventHandlers/CardTypeEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Karnak.Domain.Events;
+using MediatR;
+
+namespace Karnak.Domain.EventHandlers
+{
+    public class CardTypeEventHandler :
+        INotificationHandler<CardTypeRegisteredEvent>,
+        INotificationHandler<CardTypeUpdatedEvent>,
+        INotificationHandler<CardTypeRemovedEvent>
+    {
+        public Task Handle(CardTypeUpdatedEvent message, CancellationToken cancellationToken)
+        {
+            // Send some notification e-mail
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(CardTypeRegisteredEvent message, CancellationToken cancellationToken)
+        {
+            // Send some greetings e-mail
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(CardTypeRemovedEvent message, CancellationToken cancellationToken)
+        {
+            // Send some see you soon e-mail
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Karnak.Domain/EventHandlers/TransactionEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Karnak.Domain.Events;
+using MediatR;
+
+namespace Karnak.Domain.EventHandlers
+{
+    public class TransactionEventHandler :
+        INotificationHandler<TransactionRegisteredEvent>,
+        INotificationHandler<TransactionUpdatedEvent>,
+        INotificationHandler<TransactionRemovedEvent>
+    {
+        public Task Handle(TransactionUpdatedEvent message, CancellationToken cancellationToken)
+        {
+            // Send some notification e-mail
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(TransactionRegisteredEvent message, CancellationToken cancellationToken)
+        {
+            // Send some greetings e-mail
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(TransactionRemovedEvent message, CancellationToken cancellationToken)
+        {
+            // Send some see you soon e-mail
+
+            return Task.CompletedTask;
+        }
+    }
+}
