69edcfa950c6483a933c45456c4fd56ec1a66c53
silvastefan <silva.stefan@gmail.com>
2019-04-29 00:14:45 -0300

    add domain core events

---
+++ src/Karnak.Domain.Core/Events/IEventStore.cs
@@ -0,0 +1,7 @@
+﻿namespace Karnak.Domain.Core.Events
+{
+    public interface IEventStore
+    {
+        void Save<T>(T theEvent) where T : Event;
+    }
+}
---
+++ src/Karnak.Domain.Core/Events/IHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Karnak.Domain.Core.Events
+{
+    public interface IHandler<in T> where T : Message
+    {
+        void Handle(T message);
+    }
+}
