36f9fd06fde8e497e9c49c39c38c4b30f3a90c3f
silvastefan <silva.stefan@gmail.com>
2019-04-29 00:21:47 -0300

    add CrossCutting

---
+++ src/Karnak.Infra.CrossCutting.Identity/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Karnak.Infra.CrossCutting.Identity.Models.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
