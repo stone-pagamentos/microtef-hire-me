0c423a6ee166581a42d276a103b28dd7ae95d8c9
silvastefan <silva.stefan@gmail.com>
2019-04-29 00:12:51 -0300

    add application Interfaces

---
+++ src/Karnak.Application/Interfaces/ITransactionTypeAppService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Karnak.Application.EventSourcedNormalizers;
+using Karnak.Application.ViewModels;
+
+namespace Karnak.Application.Interfaces
+{
+    public interface ITransactionTypeAppService : IDisposable
+    {
+        void Register(TransactionTypeViewModel TransactionTypeViewModel);
+        IEnumerable<TransactionTypeViewModel> GetAll();
+        TransactionTypeViewModel GetById(Guid id);
+        TransactionTypeViewModel GetByName(string name);
+        void Update(TransactionTypeViewModel TransactionTypeViewModel);
+        void Remove(Guid id);
+        IList<TransactionTypeHistoryData> GetAllHistory(Guid id);
+    }
+}
