40455ba7883d6142099335d8d30a16908ebd2b65
silvastefan <silva.stefan@gmail.com>
2019-04-29 01:49:07 -0300

    Add project entity framework core 2.2

---
+++ Desafio_Stone/EFCoreMapStoneV13/EFCoreMapStoneV13/TransactionType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreMapStoneV13
+{
+    public partial class TransactionType
+    {
+        public TransactionType()
+        {
+            Transaction = new HashSet<Transaction>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Transaction> Transaction { get; set; }
+    }
+}
